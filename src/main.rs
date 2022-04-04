2266b7edfbe960bcc548484e0a5913b977b6b9a0
Filip Masiukiewicz <scynthero@gmail.com>
2022-04-04 13:30:06 +0200

    feat: task from second list

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,47 @@
-#![deny(warnings)]
+#![allow(warnings)]
+#![allow(dead_code)]
 extern crate mpi;
 
 use mpi::topology::SystemCommunicator;
 use mpi::traits::*;
-use rand::Rng;
+// use rand::Rng;
 
 fn main() {
     let universe = mpi::initialize().unwrap();
     let world = universe.world();
+
     match world.rank() {
         0 => {
-            let number_to_send: i64 = rand::thread_rng().gen_range(0..100);
-            println!("Sending number {} from rank {}", number_to_send, world.rank());
-            world.process_at_rank(1).send(&number_to_send);
-            let mut buf: i64 = 0;
-            world.process_at_rank(3).receive_into(&mut buf);
-            println!("Rank {} received number: {}", world.rank(), buf);
+            let mut messages: Vec<i32> = (0..world.size()).collect();
+            println!("Starting message queue contains {:?}", messages);
+            messages.reverse();
+            println!("Rank {} got message {:?}", world.rank(), messages.pop().unwrap());
+            println!("Sending message queue containing: {:?} from rank {}", messages, world.rank());
+            world.process_at_rank(1).send(&messages[..]);
         }
         1 => {
-            send_receive(0, 2, &world)
+            scatter_into(0, 2, &world)
         }
         2 => {
-            send_receive(1, 3, &world)
+            scatter_into(1, 3, &world)
         }
         3 => {
-            send_receive(2, 0, &world)
+            scatter_into(2, 0, &world)
         }
         _ => {panic!("Wrong number of ranks provided")}
     }
 }
 
-fn send_receive(receive_from: i32, send_to: i32, world: &SystemCommunicator) {
-    let mut buf: i64 = 0;
-    world.process_at_rank(receive_from).receive_into(&mut buf);
-    println!("Rank {} received number: {}", world.rank(), buf);
-    let number_to_add = rand::thread_rng().gen_range(0..100);
-    println!("Rank {} adds: {}", world.rank(), number_to_add);
-    buf += number_to_add;
-    println!("Sending number {} from rank {}", buf, world.rank());
-    world.process_at_rank(send_to).send(&buf);
+fn scatter_into(receive_from: i32, send_to: i32, world: &SystemCommunicator) {
+    // let mut messages: [i32; 4]= [0,0,0,0];
+    let (mut msg, status) = world.process_at_rank(receive_from).receive_vec::<i32>();
+    // let (mut messages, status) = world.process_at_rank(receive_from).receive::<Vec<i32>>();
+    println!("Rank {} got queue {:?}", world.rank(), msg);
+    let message = msg.pop();
+    println!("Rank {} poped message {:?}", world.rank(), message.unwrap());
+    if msg.len() > 0{
+        println!("Sending message queue containing: {:?} from rank {}", msg, world.rank());
+        world.process_at_rank(send_to).send(&msg[..]);
+    }
+
 }
