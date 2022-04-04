bbb81bd59c29ad4c11e1f36a10523734ea5fa5a5
Filip Masiukiewicz <scynthero@gmail.com>
2022-04-04 13:33:55 +0200

    fix: unused imports, code reformat

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,5 @@
-#![allow(warnings)]
-#![allow(dead_code)]
-extern crate mpi;
-
 use mpi::topology::SystemCommunicator;
 use mpi::traits::*;
-// use rand::Rng;
 
 fn main() {
     let universe = mpi::initialize().unwrap();
@@ -28,20 +23,17 @@ fn main() {
         3 => {
             scatter_into(2, 0, &world)
         }
-        _ => {panic!("Wrong number of ranks provided")}
+        _ => { panic!("Wrong number of ranks provided") }
     }
 }
 
 fn scatter_into(receive_from: i32, send_to: i32, world: &SystemCommunicator) {
-    // let mut messages: [i32; 4]= [0,0,0,0];
-    let (mut msg, status) = world.process_at_rank(receive_from).receive_vec::<i32>();
-    // let (mut messages, status) = world.process_at_rank(receive_from).receive::<Vec<i32>>();
+    let (mut msg, _status) = world.process_at_rank(receive_from).receive_vec::<i32>();
     println!("Rank {} got queue {:?}", world.rank(), msg);
     let message = msg.pop();
     println!("Rank {} poped message {:?}", world.rank(), message.unwrap());
-    if msg.len() > 0{
+    if msg.len() > 0 {
         println!("Sending message queue containing: {:?} from rank {}", msg, world.rank());
         world.process_at_rank(send_to).send(&msg[..]);
     }
-
 }
